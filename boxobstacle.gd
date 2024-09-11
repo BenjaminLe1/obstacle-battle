96ffb008fb2e77486bafe11fb1954ffc80d75a48
pkante21 <pranav.kante@newrcompany.com>
2024-09-11 10:55:37 -0400

    player 2

--- boxobstacle.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-
-class_name Obstacle
-
-var speed = 300
-
-func _process(delta):
-	position.y -= delta * speed
---
+++ obstacle.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+
+var vel = Vector2(1,0)
+
+var speed = 300
+
+func _physics_process(delta):
+	
+	var collision_info = move_and_collide(vel.normalized() * delta * speed)
+	
+	
+	
+	

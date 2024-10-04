ecba0bb2804320f031a3076d27af4cbb2fdadd31
BenjaminLe1 <le166@purdue.edu>
2024-10-03 21:32:00 -0400

    Working

--- control.gd
+++ control.gd
@@ -6,19 +6,20 @@ func _ready() -> void:
 	reset_timer()
 	pass # Replace with function body.
 
+# 0.05 SECOND INCREMENTS
 var seconds = 0
 var minutes = 0
-var Dseconds = 30
-var Dminutes = 1
+var Dseconds = 0
+var Dminutes = 0
 
 func _on_timer_timeout() -> void:
 	if seconds == 0:
 		if minutes > 0:
-			minutes -= 1
+			minutes += 1
 			seconds = 60
-	seconds -=1
+	seconds +=1
 	
-	$Label.text = str(minutes) + ": " + str(seconds)
+	$Label.text = "Score: " + str(seconds)
 	pass # Replace with function body.
 
 func reset_timer():

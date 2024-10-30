8d59e3d7afb8e01ef2bf8628672757ea8700dbc8
BenjaminLe1 <le166@purdue.edu>
2024-10-30 11:14:55 -0400

    ksl

--- Connection.cs
+++ Connection.cs
@@ -6,26 +6,21 @@ using System.IO.Ports;
 public partial class Connection : Node2D
 {
 	SerialPort serialPort;
-
 	RichTextLabel text;
+	string f = "asda";
 
 	public override void _Ready()
 	{
 		text = GetNode<RichTextLabel>("RichTextLabel");
 		serialPort = new SerialPort();
-		//serialPort.PortName = "COM3";
+		serialPort.PortName = "COM3";
 		serialPort.BaudRate = 9600;
 		serialPort.Open();
-
 	}
 
 	public override void _Process(double delta)
 	{
 		if(!serialPort.IsOpen) return;
 		string serialMessage = serialPort.ReadExisting();
-		serialMessage = "B1\n";
-		text.Text = serialMessage;
-		//serialMessage = "B1\n";
-		//append_text("B1\n");
 	}
 }
--- rich_text_label.gd
+++ rich_text_label.gd
@@ -1,10 +1,14 @@
 extends RichTextLabel
 
 func _ready() -> void:
-	var rawText = get_parsed_text()
+	var rawText = Connection.serialMessage
+	print(rawText)
 	if rawText == "B1\n":
+		print("B1 Pressed")
 		Collision.B1 = true
 	if rawText == "B2\n":
+		print("B2 Pressed")
 		Collision.B2 = true
 	if rawText == "B3\n":
+		print("B3 Pressed")
 		Collision.B3 = true
